54e96abd54edc82e0b9d60b1c882a40a546f1f27
Aratimius <aratimius@gmail.com>
2024-09-03 05:42:00 +0300

    update

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
 
 hostName = "localhost"
 serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
 
-    def __get_index(self):
-
-        with open('index.html', 'r') as file:
-            response = file.read()
-
-        return response
+    @staticmethod
+    def get_context_data():
+        """ Метод, который открывает файл на чтение данных из него"""
+        with open("index.html", "r", encoding="utf-8") as file:
+            context = file.read()
+            return context
 
     def do_GET(self):
-        query_components = parse_qs(urlparse(self.path).query)
-        page_address = query_components.get('page')
-        page_content = self.__get_index()
+        """ Метод для обработки входящих GET-запросов """
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes(page_content, "utf-8"))
+        self.wfile.write(bytes(self.get_context_data(), "utf-8"))
 
 
 if __name__ == "__main__":
+
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
+
         webServer.serve_forever()
     except KeyboardInterrupt:
+
         pass
 
     webServer.server_close()
